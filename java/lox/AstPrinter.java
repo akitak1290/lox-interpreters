5d21328f3e06eac68530e645d5ef696650250725
Kien <akitak1290@gmail.com>
2024-06-12 15:11:32 -0400

    feat: add expression class

---
+++ java/lox/AstPrinter.java
@@ -0,0 +1,69 @@
+package lox;
+
+/*
+ * A printer class mainly use to debug
+ * the abstract syntax tree class
+ */
+class AstPrinter implements Expr.Visitor<String> {
+	String print(Expr expr) {
+		return expr.accept(this);
+	}
+
+	// AstPrinter implements the visitor interface
+	// so we need to overide all of it's method,
+	// namely the visit meothod for each expression types
+	
+	@Override
+  	public String visitBinaryExpr(Expr.Binary expr) {
+    		return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+  	}
+
+  	@Override
+  	public String visitGroupingExpr(Expr.Grouping expr) {
+    		return parenthesize("group", expr.expression);
+  	}	
+
+  	@Override
+  	public String visitLiteralExpr(Expr.Literal expr) {
+    		if (expr.value == null) return "nil";
+    		return expr.value.toString();
+  	}	
+
+  	@Override
+  	public String visitUnaryExpr(Expr.Unary expr) {
+    		return parenthesize(expr.operator.lexeme, expr.right);
+  	}
+
+	/*
+	 * A helper method to wraps a name and a
+	 * list of subexpressions in parentheses
+	 * e.g (+ 1 2)
+	 */
+	private String parenthesize(String name, Expr... exprs) {
+		StringBuilder builder = new StringBuilder();
+
+		// Name of the expression
+		builder.append("(").append(name);
+		for (Expr expr : exprs) {
+			builder.append(" ");
+			builder.append(expr.accept(this));
+		}
+		builder.append(")");
+		return builder.toString();
+	}
+
+	/*
+	 * A tmp method to help debug the Ast
+	 */
+	public static void main(String[] args) {
+		Expr expression = new Expr.Binary(
+        		new Expr.Unary(
+            			new Token(TokenType.MINUS, "-", null, 1),
+            			new Expr.Literal(123)),
+        		new Token(TokenType.STAR, "*", null, 1),
+        		new Expr.Grouping(
+            			new Expr.Literal(45.67)));
+
+    		System.out.println(new AstPrinter().print(expression));
+	}
+}
---
+++ java/tool/GenerateAst.java
@@ -0,0 +1,126 @@
+package tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * A standalone application (suppose to be a script)
+ * used to generate the classes representing the
+ * abstract syntax tree to define the grammar for
+ * Lox's expression
+ *
+ * TODO: double check the format of the files to
+ * be generated, think white spaces are looking
+ * kinda ugly
+ */
+
+public class GenerateAst {
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+			System.err.println("Usage: generate_ast <output directory>");
+			System.exit(64);
+		}
+		String outputDir = args[0];
+
+		// Define name and fields for each expression class
+		defineAst(outputDir, "Expr", Arrays.asList(
+			"Binary	 : Expr left, Token operator, Expr right",
+			"Grouping: Expr expression",
+			"Literal : Object value",
+			"Unary   : Token operator, Expr right"
+		));
+
+	}
+
+	/* 
+	 * Function to generate the expression class
+	 * and subclasses, because writing boilerplate
+	 * code is boring...
+	 */
+	private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
+		String path = outputDir + "/" + baseName + ".java";
+		PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+		writer.println("package lox;");
+		writer.println();
+		writer.println("import java.util.List;");
+		writer.println();
+		writer.println("abstract class " + baseName + " {");
+
+		defineVisitor(writer, baseName, types);
+				
+		// This is why we need this class to generate
+		// the classes
+		// The AST classes
+		for (String type : types) {
+			String className = type.split(":")[0].trim();
+			String fields = type.split(":")[1].trim();
+			defineType(writer, baseName, className, fields);
+		}
+
+		// (readmore about the visitor pattern in note)
+		// The baseclass's accept method
+		writer.println();
+		writer.println("	abstract <R> R accept(Visitor<R> visitor);");
+
+		writer.println("}");
+		writer.close();
+	}
+
+	/*
+	 * Generate the visitor interface,
+	 * declare a visit method for each
+	 * expression subclass
+	 * (read more about the visitor pattern in note)
+	 */
+	private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+		writer.println(" interface Visitor<R> {");
+
+		for (String type : types) {
+			String typeName = type.split(":")[0].trim();
+			writer.println("	R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+		}
+
+		writer.println(" }");
+	}
+	
+	/*
+	 * Refactored function to be a part of defineAst()
+	 * to generate each AST classes
+	 */
+	private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+		writer.println(" static class " + className + " extends " + baseName + " {");
+
+		// Constructor
+		writer.println("	" + className + "(" + fieldList + "){");
+
+		// Store parameters in fields.
+		String[] fields = fieldList.split(", ");
+		for (String field : fields) {
+			String name = field.split(" ")[1];
+			writer.println("	this." + name + " = " + name + ";");
+		}
+
+		writer.println("	}");
+
+		// Visotor pattern.
+		// (read more about the visitor pattern in the note)
+		// each subclass override the accept method and call the
+		// right visit method for its own type
+		writer.println();
+		writer.println("	@Override");
+		writer.println("	<R> R accept(Visitor<R> visitor) {");
+		writer.println("		return visitor.visit" + className + baseName + "(this);");
+		writer.println("	}");
+
+		// Fields
+		writer.println();
+		for (String field : fields) {
+			writer.println("	final " + field + ";");
+		}
+
+		writer.println("	}");
+	}
+}
